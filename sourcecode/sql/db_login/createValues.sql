00c004d56d1b15f687954b6e28d877c7d21796ac
kronbergerindustries <martin.kronberger@gmx.at>
2021-02-21 10:37:34 +0100

    commit new order

---
+++ sourcecode/sql/db_login/createValues.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`id`, `username`, `password`) VALUES (1, 'USER', '5AHET');
+INSERT INTO `users` (`id`, `username`, `password`) VALUES (2, 'GUEST', '00000');
+INSERT INTO `users` (`id`, `username`, `password`) VALUES (3, 'ADMIN', '53AC2');
