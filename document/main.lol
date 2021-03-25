8592ae65dcc41f454c5ded43a67a9f260c78c78c
kronbergerindustries <martin.kronberger@gmx.at>
2021-03-25 18:19:01 +0100

    latex document changed

---
+++ document/main.lol
@@ -0,0 +1,1 @@
+\contentsline {lstlisting}{\numberline {V.1}Code Starten eines Threads}{12}{lstlisting.5.1}%
--- sourcecode/io/gpio.py
+++ sourcecode/io/gpio.py
@@ -14,14 +14,14 @@ pin_button_2_5_0 = 20
 pin_button_2_5_1 = 21
 
 # Definieren der Relai-Pins des ersten Relai Moduls
-pin_relai_1_1 = 11
-pin_relai_1_2 = 13
-pin_relai_1_3 = 15
-pin_relai_1_4 = 18
+pin_relais_1_1 = 11
+pin_relais_1_2 = 13
+pin_relais_1_3 = 15
+pin_relais_1_4 = 18
 
 # Definieren der Relai-Pins des zweiten Relai Moduls
-pin_relai_2_1 = 16
-pin_relai_2_2 = 24
+pin_relais_2_1 = 16
+pin_relais_2_2 = 24
 
 # Definieren der Debounce-Zeit der Taster
 debounce = 0.02
@@ -39,24 +39,24 @@ button_2_5_0 = Button(pin=pin_button_2_5_0, pull_up=None, active_state=True, bou
 button_2_5_1 = Button(pin=pin_button_2_5_1, pull_up=None, active_state=True, bounce_time=debounce)
 
 # Definieren der Objecte des ersten Relai Moduls
-relai_1_1 = DigitalOutputDevice(pin=pin_relai_1_1, active_high=True, initial_value=False, pin_factory=None)
-relai_1_2 = DigitalOutputDevice(pin=pin_relai_1_2, active_high=True, initial_value=False, pin_factory=None)
-relai_1_3 = DigitalOutputDevice(pin=pin_relai_1_3, active_high=True, initial_value=False, pin_factory=None)
-relai_1_4 = DigitalOutputDevice(pin=pin_relai_1_4, active_high=True, initial_value=False, pin_factory=None)
+relais_1_1 = DigitalOutputDevice(pin=pin_relais_1_1, active_high=True, initial_value=False, pin_factory=None)
+relais_1_2 = DigitalOutputDevice(pin=pin_relais_1_2, active_high=True, initial_value=False, pin_factory=None)
+relais_1_3 = DigitalOutputDevice(pin=pin_relais_1_3, active_high=True, initial_value=False, pin_factory=None)
+relais_1_4 = DigitalOutputDevice(pin=pin_relais_1_4, active_high=True, initial_value=False, pin_factory=None)
 
 # Definieren der Objecte des zeiten Relai Moduls
-relai_2_1 = DigitalOutputDevice(pin=pin_relai_2_1, active_high=True, initial_value=False, pin_factory=None)
-relai_2_2 = DigitalOutputDevice(pin=pin_relai_2_2, active_high=True, initial_value=False, pin_factory=None)
+relais_2_1 = DigitalOutputDevice(pin=pin_relais_2_1, active_high=True, initial_value=False, pin_factory=None)
+relais_2_2 = DigitalOutputDevice(pin=pin_relais_2_2, active_high=True, initial_value=False, pin_factory=None)
 
 class gpio(object):
 	def __init__(self):
-		self.running = True
+		self.running = None
 		self.thread = None
 	
 	def start(self):
 		self.thread = Thread(target=self.runner)
-
-		self.thread.daemon = False
+		self.thread.daemon = True
+		self.running = True
 		self.thread.start()
 
 	def stop(self):
@@ -64,29 +64,27 @@ class gpio(object):
 		self.thread.join()
 
 	def runner(self):
-		# Ansteuerung
-		# button_1_1.when_pressed = pin___.toggle
 
 		# Ansteuerung des Relais der Hupe
-		button_2_1.when_pressed = relai_2_2
-		button_2_1.when_released = relai_2_2
+		button_2_1.when_pressed = relais_2_2
+		button_2_1.when_released = relais_2_2
 
 		# Ansteuerung des Relais der Hupe
-		button_2_2.when_pressed = relai_1_1.on
-		button_2_2.when_released = relai_1_1.off
+		button_2_2.when_pressed = relais_1_1.on
+		button_2_2.when_released = relais_1_1.off
 
 		# Ansteuerung Taster zum Abblenden
-		button_2_3.when_pressed = relai_1_2.off
-		button_2_3.when_released = relai_1_2.on
+		button_2_3.when_pressed = relais_1_2.off
+		button_2_3.when_released = relais_1_2.on
 
 		# Ansteuerung des Relais des Aufbendlichts
-		button_2_4.when_pressed = relai_1_2.on
-		button_2_4.when_released = relai_1_2.off
+		button_2_4.when_pressed = relais_1_2.on
+		button_2_4.when_released = relais_1_2.off
 
 		# Ansteuerung des Relais der Hupe
-		button_2_5_0.when_pressed = relai_1_3.on
-		button_2_5_0.when_released = relai_1_3.off
+		button_2_5_0.when_pressed = relais_1_3.on
+		button_2_5_0.when_released = relais_1_3.off
 
 		# Ansteuerung des Relais der Hupe
-		button_2_5_1.when_pressed = relai_1_4.on
-		button_2_5_1.when_released = relai_1_4.off
+		button_2_5_1.when_pressed = relais_1_4.on
+		button_2_5_1.when_released = relais_1_4.off
